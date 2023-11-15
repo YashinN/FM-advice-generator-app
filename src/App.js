c751265d7ae771d5b4bc9dfa80ed88eebee406f6
Yashin <yashinnaran@gmail.com>
2023-11-15 20:54:59 +0200

    added vercel analytics tracking component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Main from "./components/Main";
 import AdviceCard from "./components/AdviceCard";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Main>
       <AdviceCard />
+      <Analytics />
     </Main>
   );
 }
