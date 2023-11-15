1a1261e56e5f42f5d287e10143056507a3f12e26
Yashin <yashinnaran@gmail.com>
2023-11-15 20:04:54 +0200

    Added loading state and animation, triggers while fetching new quote.

--- src/components/AdviceCard.js
+++ src/components/AdviceCard.js
@@ -7,16 +7,19 @@ import dividerSvg from "./svg/pattern-divider-desktop.svg";
 
 const AdviceCard = () => {
   const [count, setCount] = useState(0);
-  const { quote } = useQuote([count]);
+  const { quote, loading } = useQuote([count]);
 
   return (
     <section className={styles.adviceCard}>
       <h1 className={styles.heading}>advice #{quote?.id}</h1>
       <blockquote className={styles.quote}>" {quote?.advice} "</blockquote>
-
-      <img className={styles.divider} src={dividerSvg} alt="" />
+      <img className={styles.divider} src={dividerSvg} alt="Divider" />
       <button className={styles.btn} onClick={() => setCount(count + 1)}>
-        <img src={diceIcon} alt="Dice" />
+        <img
+          src={`${diceIcon}`}
+          className={` ${loading && styles.loading}`}
+          alt="Dice"
+        />
       </button>
     </section>
   );
--- src/hooks/useQuote.js
+++ src/hooks/useQuote.js
@@ -2,20 +2,31 @@ import { useState, useEffect } from "react";
 
 const useQuote = (dependancies = []) => {
   const [quote, setQuote] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     let randomID = Math.floor(Math.random() * 225 + 1);
+
     const getQuote = async () => {
-      const res = await fetch(`https://api.adviceslip.com/advice/${randomID}`);
-      const data = await res.json();
-      setQuote(data.slip);
-      console.log(data.slip);
+      setLoading(true);
+
+      try {
+        const res = await fetch(
+          `https://api.adviceslip.com/advice/${randomID}`
+        );
+        const data = await res.json();
+        setQuote(data.slip);
+      } catch (error) {
+        throw new Error("Problem Eish!");
+      } finally {
+        setLoading(false);
+      }
     };
 
     getQuote();
   }, [...dependancies]);
 
-  return { quote };
+  return { quote, loading };
 };
 
 export default useQuote;
