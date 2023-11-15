df61e7e2d42ae332063a7042d63c204d0087edae
Yashin <yashinnaran@gmail.com>
2023-11-15 16:22:37 +0200

    AdviceCard uses custom hook to get new quote, a new quote is fetched on click.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { useEffect, useState } from "react";
-
 import Main from "./components/Main";
 import AdviceCard from "./components/AdviceCard";
 
 function App() {
-  const [quote, setQuote] = useState();
-
-  useEffect(() => {
-    const getQuote = async () => {
-      const res = await fetch("https://api.adviceslip.com/advice");
-      const data = await res.json();
-      setQuote(data.slip);
-    };
-
-    getQuote();
-  }, []);
-
   return (
     <Main>
-      <AdviceCard quote={quote} />
+      <AdviceCard />
     </Main>
   );
 }
--- src/components/AdviceCard.js
+++ src/components/AdviceCard.js
@@ -1,15 +1,21 @@
+import useQuote from "../hooks/useQuote";
+import { useState } from "react";
+
 import styles from "./AdviceCard.module.css";
 import diceIcon from "./svg/icon-dice.svg";
 import dividerSvg from "./svg/pattern-divider-desktop.svg";
 
-const AdviceCard = ({ quote }) => {
+const AdviceCard = () => {
+  const [count, setCount] = useState(0);
+  const { quote } = useQuote([count]);
+
   return (
     <section className={styles.adviceCard}>
       <h1 className={styles.heading}>advice #{quote?.id}</h1>
-      <blockquote className={styles.quote}>{quote?.advice}</blockquote>
+      <blockquote className={styles.quote}>" {quote?.advice} "</blockquote>
 
       <img className={styles.divider} src={dividerSvg} alt="" />
-      <button className={styles.btn}>
+      <button className={styles.btn} onClick={() => setCount(count + 1)}>
         <img src={diceIcon} alt="Dice" />
       </button>
     </section>
